bdd4232ab8d89cfe0b48484bb2345ffc52ae0e10
Islom <salomshox@gmail.com>
2022-11-11 06:18:52 +0500

    Testing

--- test.py
+++ test.py
@@ -16,15 +16,14 @@ def echo(update, context):
 def main():
 
     TOKEN = os.environ.get("TOKEN")
-    APP_NAME = 'https://pythonsiut.herokuapp.com/'
 
     updater = Updater(TOKEN, use_context=True)
     updater.bot.send_message(chat_id =-321996347,  text="Salom testing hozir "+strNow)
     dp = updater.dispatcher
     
-    dp.add_handler(CommandHandler("start", start))
+    #dp.add_handler(CommandHandler("start", start))
 
-    dp.add_handler(MessageHandler(Filters.text, echo))
+    #dp.add_handler(MessageHandler(Filters.text, echo))
 
     # updater.start_webhook(listen="0.0.0.0", port=PORT, url_path = TOKEN, webhook_url=APP_NAME+ TOKEN)
     # updater.idle()
